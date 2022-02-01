d3e1240343844caf5b2a8df0708aba3e28dc1553
Illia <example@gmail.com>
2022-02-01 18:18:31 +0100

    function

--- test.js
+++ test.js
@@ -1,42 +1,9 @@
-// function sumInput() {
+'use strict';
+let showMessage;
 
-//   let numbers = [];
-
-//   while (true) {
-
-//     let value = prompt("Введите число", 0);
-
-//     // Прекращаем ввод?
-//     if (value === "" || value === null || !isFinite(value)) break;
-
-//     numbers.push(+value);
-//   }
-
-//   let sum = 0;
-//   for (let number of numbers) {
-//     sum += number;
-//   }
-//   return sum;
-// }
-
-// alert( sumInput() );
-
-function getMaxSubSum(arr) {
-  let maxSum = 0;
-  let partialSum = 0;
-
-  for (let item of arr) { // для каждого элемента массива
-    partialSum += item; // добавляем значение элемента к partialSum
-    maxSum = Math.max(maxSum, partialSum); // запоминаем максимум на данный момент
-    if (partialSum < 0) partialSum = 0; // ноль если отрицательное
-  }
-
-  return maxSum;
+if (2 > 1) {
+    showMessage = function(){
+       console.log('Massage');
+   }; 
 }
-
-alert( getMaxSubSum([-1, 2, 3, -9]) ); // 5
-alert( getMaxSubSum([-1, 2, 3, -9, 11]) ); // 11
-alert( getMaxSubSum([-2, -1, 1, 2]) ); // 3
-alert( getMaxSubSum([100, -9, 2, -3, 5]) ); // 100
-alert( getMaxSubSum([1, 2, 3]) ); // 6
-alert( getMaxSubSum([-1, -2, -3]) ); // 0
+showMessage();
