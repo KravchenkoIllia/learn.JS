6ff43db523c0abb996daf7375aa0954a8f535a7a
Illia <example@gmail.com>
2022-02-04 19:12:12 +0100

    ArraysTwo

--- arrays.js
+++ arrays.js
@@ -1,116 +1,183 @@
-// let arr = [
-//     'Illia',
-//     'Alina',
-//     'Polina',
-//     {
-//         city: 'Cherkasy',
-//         street: 'Paliohy',
-//     },
-//     true,
-//     function(){
-//         console.log('Hello, world!');
-//     }
-// ];
-// console.log(arr);
-// console.log(arr[3].city);
-// arr[5]();
-// console.log(arr.length);
+let arr = [
+    'Illia',
+    'Alina',
+    'Polina',
+    {
+        city: 'Cherkasy',
+        street: 'Paliohy',
+    },
+    true,
+    function(){
+        console.log('Hello, world!');
+    }
+];
 
-// let arrNew = arr;
-// console.log(arrNew);
-// arr[0] = 'Nikolay';
-// console.log(arr[0]);
-// arr[7] = 79;
-// console.log(arr[7]);
-
-// let arr = ['Illia', 'Alina', 'Polina',];
-// let arrNew = arr.slice(0, 1);
-// console.log(arrNew);
-// let arrOne = arr.slice();
-// console.log(arrOne);
-// let arrTwo = arr.concat('Nikolay');
-// console.log(arrTwo);
-// console.log(arr.indexOf('Illia'));
-// console.log(arr.includes('Alina'));
-
-// let arr = [
-//     {name:'Illia', age: 26},
-//     {name:'Alina', age: 29},
-//     {name:'Polina', age: 24},
-// ];
-
-// let result = arr.find(function (item, index, array) {
-//     return item.age === 29; 
-// });
-
-// console.log(result);
-
-// let resultTwo = arr.findIndex(item => item.age === 24);
-// console.log(resultTwo);
-
-// let filterResult = arr.filter(function(item, index, array){
-//     return item.age >= 29;
-// });
-
-// console.log(filterResult);
-
-// let arr = ['Illia', 'Alina', 'Polina',];
-
-// let result = arr.map(function(item, index, array){
-//     return item[0];
-// });
-
-// let resultOne = arr.map(item => item[0]);
-// console.log(result);
-// console.log(resultOne);
-// console.log(arr.reverse());
-
-// let str = 'Illia,Alina,Polina';
-
-// let arr = str.split(',');
-// console.log(arr);
-
-// let arr = ['Illia', 'Alina', 'Polina',];
-// let str = arr.join(',');
-// console.log(str);
-
-
-// let arr = ['Illia', 'Alina', 'Polina',];
-// console.log(arr.length);
-// for (let i = 0; i < arr.length; i++){
-//     console.log(arr[i]);
-// } 
-
-// for (let arrItem of arr) {
-//     console.log(arrItem);
-// }
-
-
-// let arr = ['Illia', 'Alina', 'Polina',];
-
-// arr.forEach(function(item, index,  array){
-//     console.log(`${item} stand on ${index} of ${array}`);
-// });
-
-// arr.forEach((item, index, array) => {
-//     console.log(`${item} stand on ${index} of ${array}`);
-// });
-
-
-// let arr = [1, 2, 3, 4];
-
-// let resultOne = arr.reduce(function (previousValue, item, index, array){
-//     return item + previousValue;
-// }, 0);
-
-// console.log(resultOne);
-
-// let arr = ['Illia', 'Alina', 'Polina',];
-// let newArr = arr;
-// newArr.push('Nikolay');
-// console.log(arr.length);
-
-let arr = ['Vanya', 'Ishtwan'];
-
-arr.push('Olya');
 console.log(arr);
+console.log(arr[3].city);
+arr[5]();
+console.log(arr.length);
+
+// Изминение значений
+
+let arrNew = arr;
+console.log(arrNew);
+arr[0] = 'Nikolay';
+console.log(arr[0]);
+arr[7] = 79;
+console.log(arr[7]);
+
+//Добавление, удаление элементов масива(push,pop)(shift, unsfift)
+
+//Splice Позволяет добавлять, удалять и заменять элементы
+//arr.splice(index[, deleteCount, elem1, ..., elemN])
+
+
+//Slice Создаёт новый массив в который копирует часть либо весь масив
+//arr.slice([start], [end])
+let arrSlice = ['Illia', 'Alina', 'Polina',];
+let arrNewSlice = arr.slice(0, 1);
+console.log(arrNewSlice);
+let arrSliced = arr.slice();
+console.log(arrSliced);
+
+//Concat Создаёт новый массив в который копирует данные из других массивов
+//и дополнительные значение в конец массива
+//arr.concat(arg1, arg2, ...)
+let arrTwo = arr.concat('Nikolay');
+console.log(arrTwo);
+
+//Поиск в Массиве
+
+let arrIndex = ['Illia', 'Alina', 'Polina',];
+
+//arr.indexOf(item, from) ищет item, начиная с индекса from, и возвращает
+//индекс на которым был найден элемент
+console.log(arrIndex.indexOf('Illia'));
+
+//arr.includes(item, from) ищет item, начиная с индекса from, и возвращает
+//true если поиск успешен
+let arrIncludes = ['Illia', 'Alina', 'Polina',];
+console.log(arrIncludes.includes('Alina'));
+
+//find, findIndex
+//Поиск в массиве обьектов с определённым условием
+//let result = arr.find(function(item, index, array){
+    //если true возвращается текущий элемент и перебор прерывается
+    //есои все итерации оказались ложными, возвращается undefined
+//})
+
+let arrFind = [
+    {name:'Illia', age: 26},
+    {name:'Alina', age: 29},
+    {name:'Polina', age: 24},
+];
+
+let result = arrFind.find(function (item, index, array) {
+    return item.age === 29; 
+});
+
+console.log(result);
+
+let resultTwo = arrFind.findIndex(item => item.age === 24);
+console.log(resultTwo);
+
+//filter
+//Метод ищет все элементы на которых функция колбэк вернёт true
+//let result = arr.filter(function(item, index, array){
+    //если true элемент добавляется к результату и перебор продолжается
+    //возвращает пустой массив в случае если ничего не найдено
+//})
+
+let filterResult = arrFind.filter(function(item, index, array){
+    return item.age >= 29;
+});
+
+console.log(filterResult);
+
+//Сортировка массивов
+
+//sort(fn)
+//Сортирует массив на месте меняя в нём порядок элементов
+let arrSortind = ['Illia', 'Alina', 'Polina',];
+console.log(arrSortind.sort());
+
+//Сортировка чисел
+
+let arrOfNumbers = [8, 22, 1];
+console.log(arrOfNumbers.sort());
+
+//Функция сортировки
+arrOfNumbers.sort((a, b) => a - b);
+
+//Преобразование массивов
+
+//map вызывает функцию для каждого элемента массива и возвращает
+//массив результатов выполнения этой функции
+//let result = arr.map(function(item, index, array){
+    //возвращает новое значение вместо элемента
+//})
+let mapArr = ['Illia', 'Alina', 'Polina',];
+
+let mapResult = mapArr.map(function(item, index, array){
+    return item[0];
+});
+
+let resultOne = mapArr.map(item => item[0]);
+console.log(result);
+console.log(resultOne);
+console.log(arr.reverse());
+
+
+//split, join
+//split преобразовывает строку в массив по заданому разделителю
+let str = 'Illia,Alina,Polina';
+
+let splitArr = str.split(',');
+console.log(splitArr);
+
+//join преобразовывает массив в строку с заданым разделителем
+let joinArr = ['Illia', 'Alina', 'Polina',];
+let joinStr = joinArr.join(',');
+console.log(joinStr);
+
+//Перебор элементов
+
+//FOR
+let forArr = ['Illia', 'Alina', 'Polina',];
+console.log(forArr.length);
+for (let i = 0; i < forArr.length; i++){
+    console.log(forArr[i]);
+} 
+
+//FOR ... OF
+//Можно использовать для вывода значений
+for (let arrItem of forArr) {
+    console.log(arrItem);
+}
+
+//forEach
+//Выполняет функцию для каждого элемента массива
+//arr.forEach(function(item, index, array){
+    // делать что то с item
+//})
+let eachArr = ['Illia', 'Alina', 'Polina',];
+
+eachArr.forEach(function(item, index,  array){
+    console.log(`${item} stand on ${index} of ${array}`);
+});
+
+eachArr.forEach((item, index, array) => {
+    console.log(`${item} stand on ${index} of ${array}`);
+});
+
+
+//reduce, reduceRight
+//Используются для вычисления единого значение на основе всего массива
+let reduceArr = [1, 2, 3, 4];
+
+let reduceResult = reduceArr.reduce(function (previousValue, item, index, array){
+    return item + previousValue;
+}, 0);
+
+console.log(reduceResult);
+
